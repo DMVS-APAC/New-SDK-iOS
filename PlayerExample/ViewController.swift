c557297326c54bedb7e3116277762d31755285e8
Yudhi Satrio <yudhi.satrio@dailymotion.com>
2023-05-22 09:44:48 +0700

    Initial Commit

---
+++ PlayerExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayerExample
+//
+//  Created by Yudhi SATRIO on 22/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
